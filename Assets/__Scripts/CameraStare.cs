4270466071328bd25ea44e6f8fd32bbeb4ee4f8d
Kelsey Rice <riceke13@gmail.com>
2017-10-09 23:30:10 -0700

    10.9.17 added security camera, table against wall, plants, and added (but didn't use) a painting asset set

---
+++ Assets/__Scripts/CameraStare.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraStare : MonoBehaviour {
+
+	[SerializeField] Transform target;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		//rotate camera every frame to look at player
+		transform.LookAt(target);
+		
+	}
+}
