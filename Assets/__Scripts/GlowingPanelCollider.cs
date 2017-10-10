e5622ee3af3e19f9e5735ba98b4bcc184b446633
Kelsey Rice <riceke13@gmail.com>
2017-10-09 21:09:37 -0700

    10.9.17 added script that toggles between glowing panel and black screen as well as the footprints on the floor with collider that makes the switch

---
+++ Assets/__Scripts/GlowingPanelCollider.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GlowingPanelCollider : MonoBehaviour {
+
+	[SerializeField] GameObject glowingPanel;
+	private Renderer glowingPanelRenderer;
+	[SerializeField] Material plainScreenMat;
+	[SerializeField] Material glowingScreenMat;
+	[SerializeField] float duration;
+
+
+	// Use this for initialization
+	void Start () {
+
+		glowingPanelRenderer = glowingPanel.GetComponent<Renderer> ();
+		glowingPanelRenderer.material = plainScreenMat;
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	//this is going to switch which material the glowing panel uses
+	void OnTriggerEnter(Collider other){
+		if (other.tag == "Player") {
+			//print ("trigger is working");
+//			float lerp = Mathf.PingPong(Time.time, duration) / duration;
+//			glowingPanelRenderer.material.Lerp (plainScreenMat, glowingScreenMat, lerp);
+			glowingPanelRenderer.material = glowingScreenMat;
+		}
+	}
+
+	void OnTriggerExit(Collider other){
+		if (other.tag == "Player") {
+			glowingPanelRenderer.material = plainScreenMat;
+		}
+	}
+}
